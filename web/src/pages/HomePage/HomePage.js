c6a22f222e2c4b5b438fd2d41cfcb339a885c64b
andrejpodgorsek <andrej@podgorsek.de>
2020-05-23 23:39:59 +0200

    cells

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,15 +1,18 @@
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+import BlogPostCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
   return (
     <>
       <BlogLayout>
         <h1>HomePage</h1>  
-        
-        
-        
+        <BlogPostCell></BlogPostCell>
+
+
       </BlogLayout>
-    </>
+
+      </>
+   
   )
 }
 
